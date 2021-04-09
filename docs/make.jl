b2b722e746e49f5828dfa0c3bd441ae8bcf7781c
McCoy Becker <mccoybecker@gmail.com>
2021-04-09 16:45:30 -0400

    added compat helper.

---
+++ docs/make.jl
@@ -0,0 +1,8 @@
+using CodeInfoTools
+using Documenter
+
+makedocs(; modules=[CodeInfoTools], sitename="CodeInfoTools", 
+         authors="McCoy R. Becker, Xiu-Zhe (Roger) Luo, and Valentin Churavy",
+         pages=["API Documentation" => "index.md"])
+
+deploydocs(; repo="github.com/femtomc/CodeInfoTools.jl.git", push_preview=true)
