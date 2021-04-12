b43fcc87927080b35a4fb495557c08d6a682f204
McCoy R. Becker <34410036+femtomc@users.noreply.github.com>
2021-04-11 20:18:53 -0400

    Mrb/release (#2)
    * refactor + rework interfaces to builder
    
    * Prepare for update after release.
    
    * reset to master

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,69 @@ module TestCodeInfoTools
 using CodeInfoTools
 using Test
 
+f(x) = begin
+    y = 10 + x
+    z = 20 + y
+    q = 20 + z
+    return q + 50
+end
+
+g(x) = begin
+    if x > 1
+        x + g(x - 1)
+    else
+        return 1
+    end
+    while true
+        println("Nice!")
+    end
+    return
+end
+
+@testset "code_info" begin
+    ir = code_info(f, Int)
+    @test ir == nothing
+    ir, b = code_info(f, Tuple{Int})
+    @test ir != nothing
+    display(b.ref.codelocs)
+    display(b.codelocs)
+    ir, b = code_info(g, Tuple{Int})
+    @test ir != nothing
+    display(b.ref.codelocs)
+    display(b.codelocs)
+end
+
+@testset "iterate" begin
+    ir, b = code_info(f, Tuple{Int})
+    local c = 1
+    for (v, st) in b
+        @test v == Core.SSAValue(c)
+        c += 1
+    end
+end
+
+@testset "replace!" begin
+    ir, b = code_info(f, Tuple{Int})
+    len = length(b.code)
+    for (v, st) in b
+        (st isa Expr && st.head == :call) || continue
+        replace!(b, v, Expr(:call, Base.:(*), st.args[2 : end]...))
+    end
+    @test length(b.code) == len
+    for (v, st) in b
+        st isa Expr && st.head == :call || continue
+        @test st.args[1] == Base.:(*)
+        st == Expr(:call, Base.:(*), st.args[2 : end]...)
+    end
+end
+
+@testset "insert!" begin
+    ir, b = code_info(f, Tuple{Int})
+    len = length(b.code)
+    for (v, st) in b
+        st isa Expr && st.head == :call || continue
+        replace!(b, v, Expr(:call, Base.:(*), st.args[2 : end]...))
+    end
+end
+
 end # module
