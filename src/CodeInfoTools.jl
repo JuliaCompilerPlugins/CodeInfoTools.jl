0f12357140c250ffb7f1048a8b32ab24787bb66e
McCoy Becker <mccoybecker@gmail.com>
2021-05-01 15:53:37 -0400

    Fixed docs.

--- src/CodeInfoTools.jl
+++ src/CodeInfoTools.jl
@@ -599,7 +599,7 @@ const λ = lambda
 @doc(
 """
 !!! warning
-It is relatively difficult to prevent the user from shooting themselves in the foot with this sort of functionality. Please be aware of this. Segfaults should be cautiously expected.
+    It is relatively difficult to prevent the user from shooting themselves in the foot with this sort of functionality. Please be aware of this. Segfaults should be cautiously expected.
 
 ```julia
 lambda(m::Module, src::Core.CodeInfo)
@@ -609,21 +609,21 @@ const λ = lambda
 
 Create an anonymous `@generated` function from a piece of `src::Core.CodeInfo`. The `src::Core.CodeInfo` is checked for consistency by [`verify`](@ref).
 
-        `lambda` has a 2 different forms. The first form, given by signature:
+`lambda` has a 2 different forms. The first form, given by signature:
 
-        ```julia
-        lambda(m::Module, src::Core.CodeInfo)
-        ```
+```julia
+lambda(m::Module, src::Core.CodeInfo)
+```
 
-        tries to detect the correct number of arguments automatically. This may fail (for any number of internal reasons). Expecting this, the second form, given by signature:
+tries to detect the correct number of arguments automatically. This may fail (for any number of internal reasons). Expecting this, the second form, given by signature:
 
-            ```julia
-            lambda(m::Module, src::Core.CodeInfo, nargs::Int)
-            ```
+```julia
+lambda(m::Module, src::Core.CodeInfo, nargs::Int)
+```
 
-            allows the user to specify the number of arguments via `nargs`.
+allows the user to specify the number of arguments via `nargs`.
 
-            `lambda` also has the shorthand `λ` for those lovers of Unicode.
+`lambda` also has the shorthand `λ` for those lovers of Unicode.
 """, lambda)
 
 end # module
